9b3c6f99b7345fb6f2ed1c0ba5607c5d7469deff
zhouxuan77 <zhouxuan77@163.com>
2023-02-19 17:05:29 +0800

    feat(*): 添加 Config 配置管理项

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+from config.config import ConfigUtils, ConfigError
+
+root_path = {
+    "tmp_root": 'tmp',
+    "tmp_convert": 'tmp/convert',
+    "tmp_upload": 'tmp/upload'
+}
+
+config = ConfigUtils()
